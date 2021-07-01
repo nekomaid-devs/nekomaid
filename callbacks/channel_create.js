c2d06212f5941c83fef2cba00aa5695e80a283fc
LamkasDev <lamkascz1@gmail.com>
2021-07-01 21:55:06 +0200

    Fix creating overwrites for new channels

--- callbacks/channel_create.js
+++ callbacks/channel_create.js
@@ -18,18 +18,18 @@ module.exports = {
             return;
         }
 
-        /*var serverConfig = await bot.ssm.server_fetch.fetch(bot, { type: "server_channel_create", id: channel.guild.id });
-        if(serverConfig.muteRoleID !== "-1") {
-            var muteRole = await channel.guild.roles.fetch(serverConfig.muteRoleID).catch(e => { console.log(e); });
-            if(muteRole !== undefined) {
+        let server_config = await global_context.neko_modules_clients.ssm.server_fetch.fetch(global_context, { type: "server_channel_create", id: channel.guild.id });
+        if(server_config.muteRoleID !== "-1") {
+            let mute_role = await channel.guild.roles.fetch(server_config.muteRoleID).catch(e => { console.log(e); });
+            if(mute_role !== undefined) {
                 try {
                     if(channel.type === "text") {
-                        channel.createOverwrite(muteRole, {
+                        channel.createOverwrite(mute_role, {
                             SEND_MESSAGES: false,
                             ADD_REACTIONS: false
                         }).catch(e => { console.log(e); });
                     } else if(channel.type === "voice") {
-                        channel.createOverwrite(muteRole, {
+                        channel.createOverwrite(mute_role, {
                             CONNECT: false,
                             SPEAK: false
                         }).catch(e => { console.log(e); });
@@ -38,6 +38,6 @@ module.exports = {
                     console.log("[mod] Skipped a permission overwrite because I didn't have permission-")
                 }
             }
-        }*/
+        }
     }
 }
