9f20d48fdcab108df11a5231918cb12a54287ba5
LamkasDev <lamkascz1@gmail.com>
2021-07-01 23:03:58 +0200

    Update marriage_proposal.js

--- scripts/helpers/marriage_proposal.js
+++ scripts/helpers/marriage_proposal.js
@@ -1,10 +1,10 @@
 class MarriageProposal {
     constructor() {
-        this.sourceID = -1;
-        this.sourceDisplayName = -1;
-        this.targetID = -1;
-        this.targetDisplayName = -1;
-        this.channelID = -1;
+        this.source_ID = -1;
+        this.source_tag = -1;
+        this.target_ID = -1;
+        this.target_tag = -1;
+        this.channel_ID = -1;
     }
 }
 
